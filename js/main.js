2413fb99a1e9dd33fb3e2239ad8370ea75b4be2b
João Jilberto Leal <112709914+joaojilbertogil@users.noreply.github.com>
2023-06-26 20:58:30 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//capturar dados do formulário
+const pega = document.querySelector('#formulario')
+
+pega.addEventListener('submit', function(event){
+    event.preventDefault()
+    console.log('teste')
+})
